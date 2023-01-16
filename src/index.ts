e582292cad24c85e10971fd28ea5b60a8f50302f
brunonasc74 <bruno.hpn74@gmail.com>
2023-01-16 14:18:52 -0300

    Express server created, initial configuration set

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('a');
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
