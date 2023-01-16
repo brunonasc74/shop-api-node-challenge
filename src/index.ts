c309363dd0b4a071b0112ee5a08f5cbe9b878478
brunonasc74 <bruno.hpn74@gmail.com>
2023-01-16 15:00:38 -0300

    Swagger documentation implemented, swagger.json file created

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import router from './router';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDoc from './doc/swagger.json';
 
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.get('/', (_, res) => res.redirect('/api-docs'));
 
 router(app);
 
